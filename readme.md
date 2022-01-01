1c02b0f094aeee820267375ddd3a463dac770727
Jimmy Moon <ragingwind@gmail.com>
2022-01-02 08:04:43 +0900

    Update test script

--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-# project
+# Github Action Templates (for me)
+
+> Github Action Template (for me), All of scriptes are able to test with [act in local](https://github.com/nektos/act)
+
+# Usese
+
+```sh
+# test all with workflow_dispatch event
+./test.sh
+
+# test a job in workflow
+./test.sh rust-toolchain
+```
+
+# License
+
+MIT @ Jimmy
--- test.sh
+++ test.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-act workflow_dispatch --container-architecture linux/amd64
+jobs=('rust')
+
+if [[ $(uname -m) == 'arm64' ]]; then
+  CONTAINER_ARCH="--container-architecture linux/amd64"
+fi
+
+test_all() {
+  act workflow_dispatch $CONTAINER_ARCH --env-file act.env
+}
+
+test_job() {
+  ENVFILE="--env-file act.env"
+  echo "Testing with act -j $1 $CONTAINER_ARCH $ENVFILE"
+  act -j $1 $CONTAINER_ARCH $ENVFILE
+}
+
+[[ -z $1 ]] && test_all || test_job $1
